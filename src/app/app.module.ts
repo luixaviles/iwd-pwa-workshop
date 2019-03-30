f729fc88656afc9f66dde2f9c45e88dee864967e
luixaviles <luis.avilesz@gmail.com>
2019-03-30 11:31:16 -0400

    Generando arquitectura modular y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from './login/login.module';
+import { ContactModule } from './contact/contact.module';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoginModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
